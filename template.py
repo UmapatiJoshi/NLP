a20e540357a7b980207612f40d94a9704ecc2344
UmapatiJoshi <umapati21joshi@gmail.com>
2024-02-07 15:46:18 +0530

    Initializing the project

---
+++ template.py
@@ -0,0 +1,56 @@
+import os
+from pathlib import Path
+import logging
+from datetime import datetime
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path = os.path.join(os.getcwd(), 'logs', LOG_FILE)
+os.makedirs(logs_path, exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s -%(message)s",
+    level=logging.INFO,
+)
+
+project_name = 'textSummarizer'
+
+list_of_files = [
+    '.github/workflows/.gitkeep',
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/logging/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constant/__init__.py",
+    'config/config.yaml',
+    'params.yaml',
+    'app.py',
+    'main.py',
+    'Dockerfile',
+    'requirements.txt',
+    'setup.py',
+    'research/trials.ipynb'
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    if filedir != '':
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f'Creating directory: {filedir} for the file {filename}')
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath,'w') as f:
+            pass
+            logging.info(f'Creating empty file: {filepath} ')
+
+    else:
+        logging.info(f'{filepath} already exists')
+
